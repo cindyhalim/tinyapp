fc6c8726127ab193a0e6aa7092d7c3d810bd10bc
cindyhalim <jcindyhalim@gmail.com>
2019-09-02 17:48:25 +0000

    added new route to submission form

--- express_server.js
+++ express_server.js
@@ -20,17 +20,14 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
 app.get('/urls/:shortURL', (req, res) => {
   let templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render('urls_show', templateVars)
 
-})
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
 app.listen(PORT, () => {
