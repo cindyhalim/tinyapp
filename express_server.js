e2ecd2b2d8b56c2884544c374476a5e15344a241
cindyhalim <jcindyhalim@gmail.com>
2019-09-03 23:16:38 +0000

    Created registration page, registration handler, and handled registration errors

--- express_server.js
+++ express_server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+//MIDDLEWARE
+
 //set view engine to ejs
 app.set('view engine', 'ejs');
 //use res.render
 
-//cookies
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 
@@ -14,11 +15,27 @@ app.use(cookieParser());
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+//DATABASE
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouse.ca',
   '9sm5xK': 'http://www.google.com'
 };
 
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
+
+//REQUESTS
 app.get('/', (req, res) => {
   res.send('Hello!');
 });
@@ -70,11 +87,29 @@ app.post('/logout', (req, res) => {
   res.redirect('/urls');
 })
 
+app.get('/register', (req, res) => {
+  let templateVars = {username: req.cookies['username']};
+  res.render('urls_register', templateVars);
+})
+
+app.post('/register', (req, res) => {
+  let userID = generateRandomId(req.body.email);
+  if (req.body.email === '' || req.body.password === '' || emailExists(req.body.email, users)) {
+    res.statusCode = 400;
+    res.send(`Error ${res.statusCode}`);
+  } else {
+    users[userID] = {id: userID, email: req.body.email, password: req.body.password}
+    res.cookie('user_id', users[userID].id);
+    res.redirect('/urls');
+    console.log(users);
+  }
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
 });
 
-function generateRandomString() {
+const generateRandomString = () => {
     let randomString = '';
     let numbers = '1234567890'
     let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
@@ -82,4 +117,23 @@ function generateRandomString() {
       randomString += characters.charAt(Math.floor(Math.random() * characters.length)) + numbers.charAt(Math.floor(Math.random() * numbers.length));
     }
     return randomString;
+}
+
+const generateRandomId = (email) => {
+  let randomId = '';
+  let numbers = '1234567890'
+  randomId = email.split('@')[0]
+  for (let i = 0; i < 3; i++) {
+    randomId += i;
+  }
+  return randomId;
+}
+
+const emailExists = (email, data) => {
+  for (let obj in data) {
+    if (email === users[obj].email) {
+      return true;
+    }
+  }
+  return false;
 }
