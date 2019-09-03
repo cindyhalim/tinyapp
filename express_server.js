0ea1b10d4cb214faa1d0b683998886214877b5b1
cindyhalim <jcindyhalim@gmail.com>
2019-09-03 17:01:05 +0000

    Added edit feature to update URL

--- express_server.js
+++ express_server.js
@@ -32,9 +32,22 @@ app.get('/urls/new', (req, res) => {
   res.render('urls_new');
 });
 
+
 app.get('/urls/:shortURL', (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render('urls_show', templateVars)
+});
+
+app.post('')
+
+app.post('/urls/:shortURL/delete', (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect('/urls');
+});
+
+app.post('/urls/:shortURL', (req, res) => {
+  urlDatabase[req.params.shortURL] = req.body.longURL;
+  res.redirect('/urls');
 });
 
 app.get('/u/:shortURL', (req, res) => {              
