fed4106bcf57ff39adfa9b5cf74971f731756cd2
cindyhalim <jcindyhalim@gmail.com>
2019-10-21 08:04:34 +0000

    Style index, show, and new pages

--- express_server.js
+++ express_server.js
@@ -36,7 +36,7 @@ app.use(
 );
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname+ "/public"));
 app.use(methodOverride("_method"));
 
 // USER
